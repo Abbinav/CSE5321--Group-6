11e41bdc7c2487476cc7ff60799ebd68b14d138d
Abbinav <abbinav97@gmail.com>
2022-02-20 17:23:29 -0600

    Notification Alert

--- CalenderApp/src/main/java/com/group6/calendar/controller/CalenderController.java
+++ CalenderApp/src/main/java/com/group6/calendar/controller/CalenderController.java
@@ -1,9 +1,12 @@
 package com.group6.calendar.controller;
 
 import java.text.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -31,23 +34,41 @@ public class CalenderController {
 		model.addAttribute("title", "test");
 		List<Activity> activityData = new ArrayList<>();
 		activityData = repo.findAll();
+		List<Activity> notificationData = new ArrayList<>();
+
+		DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern(DATE_FORMATTER);
+		LocalDateTime today = LocalDateTime.now();
+		LocalDateTime tomorrow = today.plusDays(1);
+		String todayFormattedDate = today.format(inputFormat);
+		String tomorrowFormattedDate = tomorrow.format(inputFormat);
+		LocalDateTime todayAt9 = LocalDate.now().atTime(4, 0);
+
+
 		//modelData.put("activityData", activityData);
 		
 		if(activityData != null && activityData.size() > 0)
 		{
+			notificationData = activityData.stream().filter(a -> 
+			(a.getStartTime().format(inputFormat).contains(todayFormattedDate) 
+					|| a.getEndTime().format(inputFormat).contains(tomorrowFormattedDate))).collect(Collectors.toList());
+			
+			
 			for(Activity activity: activityData)
 			{
 				DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMATTER);
-				String formattedDate = activity.getStartTime().format(formatter);
+				String formattedDate = activity.getStartTime().format(formatter);			
 				//String dat1=output.format(activity.getStartTime());
 				model.addAttribute("date", formattedDate);
 				model.addAttribute("title", activity.getActivityName());				
 				modelData.put("title", activity.getActivityName());
 				modelData.put("date", formattedDate);
+				//System.out.println("notificationdata" + notificationData);
 				System.out.println("date" + formattedDate);
 				System.out.println("name" + activity.getActivityName());
 			}
 			model.addAttribute("modelData", activityData);
+			if(today.isAfter(todayAt9))
+				model.addAttribute("notificationData", notificationData);
 		}
 		
 		
@@ -56,30 +77,27 @@ public class CalenderController {
 
 
 	@RequestMapping("/CreateEvent")
-	public String gre1ting(Model model)  {
-		//,@RequestParam(value="dateEvent") String dateEvent,@RequestParam(value="nameEvent") String nameEvent
-		//System.out.println("Inside eventsToday / dateEvent :" + dateEvent);
-		//System.out.println("Inside eventsToday / nameEvent :" + nameEvent);
-
-
+	public String gre1ting(Model model)  {		
 		return "CreateEvent";
 	}
 
 
 
 	@PostMapping("/calendar/{id}")
-	public String gre11ting(Model model,@RequestParam(value="dateEvent") String dateEvent,@RequestParam(value="nameEvent") String nameEvent)  {
+	public String gre11ting(Model model,@RequestParam(value="dateEvent") String dateEvent,@RequestParam(value="endDateEvent") String endDateEvent,@RequestParam(value="nameEvent") String nameEvent)  {
 		//,@RequestParam(value="dateEvent") String dateEvent,@RequestParam(value="nameEvent") String nameEvent		
 		Activity activityData = new Activity();
 		
 		System.out.println("Inside eventsToday / dateEvent :" + dateEvent);
+		System.out.println("Inside eventsToday / dateEvent :" + endDateEvent);
+
 		System.out.println("Inside eventsToday / nameEvent :" + nameEvent);
-		if((dateEvent != null || dateEvent != "") && (nameEvent !=null || nameEvent !=""))
+		if((dateEvent != null || dateEvent != "") && (nameEvent !=null || nameEvent !="") && (endDateEvent != null || endDateEvent != ""))
 		{
 			activityData.setActivityName(nameEvent);
 			DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
 			LocalDateTime startTime = LocalDateTime.parse(dateEvent, inputFormat);
-			LocalDateTime endTime = startTime.plusHours(1);
+			LocalDateTime endTime = LocalDateTime.parse(endDateEvent, inputFormat);
 			activityData.setStartTime(startTime);;
 			activityData.setEndTime(endTime);
 			repo.save(activityData);
@@ -89,20 +107,6 @@ public class CalenderController {
 		return "redirect:/calendar";
 	}
 
-//	@GetMapping("/calendar/date/{date1}/title/{title}")
-//	public String greeeting(Model model,@PathVariable("date1") String date1,@PathVariable("title") String title) throws ParseException {
-//		model.addAttribute("something", "Abbi");
-//		SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
-//		SimpleDateFormat output = new SimpleDateFormat("yyyy-MM-dd");
-//
-//		Date d1 = null;
-//		d1=input.parse(date1);
-//		String dat1=output.format(d1);
-//
-//		model.addAttribute("date", dat1);
-//		model.addAttribute("title", title);		
-//		return "Calender";
-//	}
 
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # CSE5321--Group-6
+
+Steps to run the project:
+
+Clone the Repo
+Build & Clean the Project
+Run Configurations -> SpringBoot App
+Once the Tomcat Server has started, go to http://localhost:8087/calendar
+
+
+Database:
+We are utilizing a H2 Relationship Database Management System. The database content could be accessed using:
+
+1.http://localhost:8087/h2-console
+2.To login into DB: 
+	Driver Class:org.h2.Driver
+	JDBC URL: jdbc:h2:mem:testdb
+	User Name: sa
+	No Password required
+	Connect
+3. The DB Consists of an Activity Table that stores all the events created on the screen. There are 4 columns in the table:
+	-ID
+	-ACTIVITY_NAME
+	-END_TIME
+	-START_TIME
+
+
